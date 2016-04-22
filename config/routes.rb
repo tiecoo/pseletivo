59a55cf767da6acb4342a2bf748ff235ffd50069
Diego <diego.pereirasilva@live.com>
2016-04-22 01:03:36 -0300

    Root alterado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :courses
   resources :students
-  resources :classrooms
+  root to: 'courses#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
